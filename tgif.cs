6eb76b8170124a1950b55108c7d2cbbf60dc694d
CireWire <djcire956@gmail.com>
2018-06-14 22:11:05 -0500

    Create tgif.cs

---
+++ tgif.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Working late today, but I wanted to share this with ya'll!
+
+namespace today
+{
+  class Program
+  {
+    static void Main()
+    {
+      string day;
+
+      Console.WriteLine("What will be tomorrow be?");
+      
+      day = Console.ReadLine();
+
+      Console.WriteLine("{0} will be a great day for beer and videogames!", day);
+    }
+  }
+}
